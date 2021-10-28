d1741c16ae7eb67c381b0374f0ea908b55c2d984
Mark <mark@opifex.ca>
2021-10-27 23:49:48 -0400

    Updates to allow read and write with just the serial struct

--- src/lib.rs
+++ src/lib.rs
@@ -13,36 +13,23 @@ pub use serial::Parity;
 pub use serial::PortSettings;
 pub use serial::StopBits;
 
-use std::sync::{Arc, Mutex};
-
 /// Newtype over [`serial-rs`](https://crates.io/crates/serial)'s serial port abstraction.
-pub struct Serial(Arc<Mutex<serial::SystemPort>>);
-
-pub struct Tx(Arc<Mutex<serial::SystemPort>>);
-
-pub struct Rx(Arc<Mutex<serial::SystemPort>>);
+struct Serial(serial::SystemPort);
 
 impl Serial {
-    pub fn new<T: AsRef<OsStr> + ?Sized>(
-        port: &T,
-        settings: &serial::PortSettings,
-    ) -> serial::Result<Self> {
+    pub fn new<PORT: AsRef<OsStr> + ?Sized> (port: &PORT, settings: &serial::PortSettings) -> serial::Result<Self> {
         let mut port = serial::open(&port)?;
         port.configure(settings)?;
-        Ok(Serial(Arc::new(Mutex::new(port))))
-    }
-
-    pub fn split(self) -> (Tx, Rx) {
-        (Tx(Arc::clone(&self.0)), Rx(Arc::clone(&self.0)))
+        Ok(Serial(port))
     }
 }
 
-impl hal::serial::Read<u8> for Rx {
+impl embedded_hal::serial::Read<u8> for Serial {
     type Error = serial::Error;
 
     fn read(&mut self) -> nb::Result<u8, Self::Error> {
         let mut buf: [u8; 1] = [0];
-        match (*self.0).lock().unwrap().read(&mut buf) {
+        match self.0.read(&mut buf) {
             Ok(_) => Ok(buf[0]),
             Err(e) => match e.kind() {
                 std::io::ErrorKind::WouldBlock => Err(nb::Error::WouldBlock),
@@ -56,11 +43,11 @@ impl hal::serial::Read<u8> for Rx {
     }
 }
 
-impl hal::serial::Write<u8> for Tx {
+impl embedded_hal::serial::Write<u8> for Serial {
     type Error = serial::Error;
 
     fn write(&mut self, byte: u8) -> nb::Result<(), Self::Error> {
-        match (*self.0).lock().unwrap().write(&[byte]) {
+        match self.0.write(&[byte]) {
             Ok(_) => Ok(()),
             Err(e) => Err(nb::Error::Other(serial::Error::new(
                 serial::ErrorKind::Io(e.kind()),
@@ -70,7 +57,7 @@ impl hal::serial::Write<u8> for Tx {
     }
 
     fn flush(&mut self) -> nb::Result<(), Self::Error> {
-        match (*self.0).lock().unwrap().flush() {
+        match self.0.flush() {
             Ok(_) => Ok(()),
             Err(e) => Err(nb::Error::Other(serial::Error::new(
                 serial::ErrorKind::Io(e.kind()),
